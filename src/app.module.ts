6926598bb8e1b670e1f73d2e576b4797ff976c53
Jonathan Baez <jbaez9910@gmail.com>
2023-01-25 12:38:29 -0500

    se agregan validaciones y demas dtos, controllers,services,entitys

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { AppService } from './app.service';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
+import { BrandsController } from './controllers/brands.controller';
+import { UsersController } from './controllers/users.controller';
+import { CustomersController } from './controllers/customers.controller';
+import { CategoriesService } from './services/categories.service';
+import { BrandsService } from './services/brands.service';
+import { UsersService } from './services/users.service';
+import { CustomersService } from './services/customers.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, CategoriesController, ProductsController],
-  providers: [AppService, ProductsService],
+  controllers: [AppController, CategoriesController, ProductsController, BrandsController, UsersController, CustomersController],
+  providers: [AppService, ProductsService, CategoriesService, BrandsService, UsersService, CustomersService],
 })
 export class AppModule {}
