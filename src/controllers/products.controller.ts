85cf9f74dd4c07a0c92c53b52095abc67ce52d5a
Jonathan Baez <jbaez9910@gmail.com>
2023-01-25 10:39:39 -0500

    Se agregaron los servicios de products

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -9,39 +9,32 @@ import {
   Delete,
 } from '@nestjs/common';
 
+import { ProductsService } from './../services/products.service';
 @Controller('products')
 export class ProductsController {
+  constructor(private productService: ProductsService) {}
   @Get()
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return {
-      message: `product ${brand} ${limit} ${offset}`,
-    };
+    return this.productService.findAll();
   }
 
   @Get(':productId')
   getProduct(@Param('productId') productId: string) {
-    return `product ${productId}`;
+    return this.productService.findOne(+productId);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'accion de crear',
-      payload,
-    };
+    return this.productService.create(payload);
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() payload: any) {
-    return {
-      message: 'accion de editar',
-      id,
-      payload,
-    };
+    return this.productService.update(+id, payload);
   }
   @Delete(':id')
   delete(@Param('id') id: number) {
